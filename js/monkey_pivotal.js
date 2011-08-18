ef9f7b10127d35d94d0d4ff75ea09042affe4c32
Alexey <leopard.not.a@gmail.com>
2011-08-18 18:48:07 +0300

    fix js

--- js/monkey_pivotal.js
+++ js/monkey_pivotal.js
@@ -1,7 +1,13 @@
 var MonkeyPivotal = {
 	current_tab_url: null,
 	bg_page: null,
+	doc_key: null,
 	gdoc: null,
+	global_counter: 0,
+	gmatches: [],
+	
+	pivotal_regex_g: /https:\/\/www.pivotaltracker.com\/story\/show\/([\d]+)([\s]?)(\([\w\s]+\))?/gi,
+	pivotal_regex: /https:\/\/www.pivotaltracker.com\/story\/show\/([\d]+)([\s]?)(\([\w\s]+\))?/i,
 	
 	init: function(){
 		this.bg_page = chrome.extension.getBackgroundPage();
@@ -19,6 +25,9 @@ var MonkeyPivotal = {
 	bind_elements: function(){
 		$('#begin_analyze').click(function(event){
 			MonkeyPivotal.begin_analyze();
+		});
+		$('#start_update').click(function(event){
+			MonkeyPivotal.start_update_document();
 		});
 	},
 	
@@ -42,7 +51,8 @@ var MonkeyPivotal = {
 			if (regex_pattern.test(this.current_tab_url)){
 				var found_str = this.current_tab_url.match(regex_pattern);
 				if (found_str[3]){
-					this.download_gdocument(found_str[3]);
+					this.doc_key = found_str[3];
+					this.download_gdocument(this.doc_key);
 				} else {
 					this.show_message('Please, go to google document');
 				}
@@ -55,21 +65,94 @@ var MonkeyPivotal = {
 	},
 	
 	download_gdocument: function(key){
-		var url = 'https://docs.google.com/feeds/download/documents/Export?docID=' + key + '&exportFormat=html&format=html';
-		var params = {
-	    'method': 'GET',
-	    'headers': {
-	      'GData-Version': '3.0'
-	    },
-			'parameters': {'alt': 'json'}
-	  };
-		this.bg_page.oauth.sendSignedRequest(url, MonkeyPivotal.handle_dowload_success, params);
+		var url = 'https://docs.google.com/feeds/download/documents/export/Export?id=' + key;
+		$.get(url, function(data){ 
+			MonkeyPivotal.handle_dowload_success(data);
+		});
 	},
 	
-	handle_dowload_success: function(resp, xhr){
-		this.show_message("test");
-		//var json_data = JSON.parse(resp).entry;
-		//this.show_message(json_data);
+	handle_dowload_success: function(response){
+		this.gdoc = response;
+		var count_links = this.gdoc.match(this.pivotal_regex_g).length;
+		if (count_links > 0){
+			this.show_message('Found ' + count_links + ' link(s).');
+			this.show_start_button();
+		} else {
+			this.show_message('Document doesn\'t have pivotal links.');
+		}
+	},
+	
+	show_start_button: function(){
+		$('#start_update').show();
+	},
+	
+	start_update_document: function(){
+		if (this.gdoc != null){
+			this.gmatches = this.gdoc.match(this.pivotal_regex_g);
+			this.global_counter = 0;
+			this.update_local_doc();
+		} else {
+			this.show_message('Analyze doc before.');
+		}
+	},
+	
+	update_local_doc: function(){
+		if (this.gmatches[this.global_counter]){
+			var temp_match = this.gmatches[this.global_counter];
+			var pivotal_ids = temp_match.match(this.pivotal_regex);
+			$.getJSON('http://monkey.railsware.com/pivotal_story_status/' + pivotal_ids[1] + '.json', function(data) {
+				if (data.story_status != 'unknown'){
+					var r = new RegExp("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/" + pivotal_ids[1] + "([\\s]?)(\\([\\w\\s]+\\))?", "gi");
+					MonkeyPivotal.gdoc = MonkeyPivotal.gdoc.replace(r, 'https://www.pivotaltracker.com/story/show/' + pivotal_ids[1] + ' (' + data.story_status + ')');
+				}
+				MonkeyPivotal.global_counter++;
+				MonkeyPivotal.update_local_doc();
+			});
+		} else {
+			this.show_message("Done. Updating doc.");
+			this.update_gdocument();
+		}
+	},
+	
+	update_gdocument: function(){
+		var params = {
+	    'method': 'PUT',
+	    'headers': {
+	      'GData-Version': '3.0',
+	      'Content-Type': 'multipart/related; boundary=END_OF_PART',
+	      'If-Match': '*'
+	    },
+			'parameters': {'alt': 'json'},
+	    'body': this.construct_content_body()
+	  };
+
+	  var url = this.bg_page.DOCLIST_FEED + this.doc_key;
+	  this.bg_page.oauth.sendSignedRequest(url, this.show_message, params);
+	},
+	
+	construct_content_body: function(){
+		var body = ['--END_OF_PART\r\n',
+	              'Content-Type: application/atom+xml;\r\n\r\n',
+	              this.construct_header_body(), '\r\n',
+	              '--END_OF_PART\r\n',
+	              'Content-Type: text/html\r\n\r\n',
+	              this.gdoc, '\r\n',
+	              '--END_OF_PART--\r\n'].join('');
+	  return body;
+	},
+	
+	construct_header_body: function(){
+		var starCat = ['<category scheme="http://schemas.google.com/g/2005/labels" ',
+	                 'term="http://schemas.google.com/g/2005/labels#starred" ',
+	                 'label="starred"/>'].join('');
+
+	  var atom = ["<?xml version='1.0' encoding='UTF-8'?>", 
+	              '<entry xmlns="http://www.w3.org/2005/Atom">',
+	              '<category scheme="http://schemas.google.com/g/2005#kind"', 
+	              ' term="http://schemas.google.com/docs/2007#document"/>',
+	              '',
+	              '</entry>'].join('');
+	  return atom;
 	},
 	
 	show_message: function(msg){
